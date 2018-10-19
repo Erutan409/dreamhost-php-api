e0974555277236f1837f3fb985660b54ba82225c
Michael Keyser <erutan409@hotmail.com>
2018-10-19 19:11:13 -0400

    * Fleshing out factory pattern

--- Dreamhost/CLI.php
+++ Dreamhost/CLI.php
@@ -7,6 +7,21 @@ use League\CLImate\CLImate;
 
 abstract class CLI
 {
+    private static $cli = null;
+
+    /**
+     * Undocumented function
+     *
+     * @return CLImate
+     */
+    public function getInstance() {
+        if (is_null(self::$cli)) {
+            self::$cli = new CLImate();
+        }
+
+        return self::$cli;
+    }
+
     public static function getCommands()
     {
         $commands = ClassFinder::getClassesInNamespace(__NAMESPACE__ . '\\Command');
