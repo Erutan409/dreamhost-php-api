64d0becc8b22c1d6a3376410a3c201d5332461db
Michael Keyser <Erutan409@users.noreply.github.com>
2018-10-20 08:54:30 -0400

    * Fleshing out

--- Dreamhost/CLI.php
+++ Dreamhost/CLI.php
@@ -10,18 +10,57 @@ abstract class CLI
     private static $cli = null;
 
     /**
-     * Undocumented function
+     * Get CLImate instance.
      *
+     * @param boolean $something
      * @return CLImate
      */
-    public function getInstance() {
+    final public static function getInstance($something = true)
+    {
         if (is_null(self::$cli)) {
             self::$cli = new CLImate();
+            self::$cli->addArt(__DIR__ . '/CLI/art/');
         }
 
         return self::$cli;
     }
 
+    /**
+     * Bootstrap CLI.
+     *
+     * @return void
+     */
+    final public static function run()
+    {
+        $climate = self::getInstance();
+        $climate->clear();
+
+        $climate->animation('dreamhost')->enterFrom('bottom');
+
+        sleep(2);
+
+        $climate->clear();
+
+        $climate->animation('dreamhost')->exitTo('top');
+
+        $climate->clear();
+
+        while (true) {
+            self::outputOrderedList(self::getCommands());
+
+            while (intval($response = $climate->input('Select command:')->prompt()) === 0) {
+                $climate->input('Please select a number from the list given.')->prompt();
+            }
+
+            sleep(2);
+        }
+    }
+
+    /**
+     * Get available commands.
+     *
+     * @return string[]
+     */
     public static function getCommands()
     {
         $commands = ClassFinder::getClassesInNamespace(__NAMESPACE__ . '\\Command');
@@ -33,10 +72,10 @@ abstract class CLI
         return Helpers::array_base_one($commands);
     }
 
-    public static function outputOrderedList(array $list, CLImate $climate)
+    public static function outputOrderedList(array $list)
     {
         foreach ($list as $number => $value) {
-            $climate->out($number . '. ' . $value);
+            self::getInstance()->out($number . '. ' . $value);
         }
     }
 }
--- Dreamhost/Helpers.php
+++ Dreamhost/Helpers.php
@@ -4,6 +4,12 @@ namespace Dreamhost;
 
 abstract class Helpers
 {
+    /**
+     * Reindex array to start at `1`.
+     *
+     * @param array $array
+     * @return $array
+     */
     final public static function array_base_one(array $array)
     {
         $index = 1;
@@ -14,5 +20,23 @@ abstract class Helpers
         }
 
         return $new;
+    }
+
+    /**
+     * Test that each element in the array evaluates to true.
+     *
+     * @param array $array
+     * @param callable $callable
+     * @return bool
+     */
+    final public static function array_every(array $array, callable $callable)
+    {
+        foreach ($array as $value) {
+            if (!$callable($value)) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
